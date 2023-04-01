5a0e7b532ddfe557ea8e50507496ad3f77d3f519
kumarpanu <kapnuganti82@gmail.com>
2023-04-01 07:40:59 +0000

    first names of people

---
+++ names/index.js
@@ -0,0 +1,8 @@
+const cityPeople = require("./country/state/city/index.js");
+const utilities = require("./utilities/utils/index.js");
+
+function getPeopleInCity(cityPeople) {
+  return cityPeople.utilities;
+}
+
+module.exports = getPeopleInCity;
